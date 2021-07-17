2de29ef5238b3dd60457dd7b8677d6c4081b6731
Alibek <alibek@192.168.88.90>
2021-07-17 18:28:10 +0600

    for_loop

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,2 +1,8 @@
+dict = {'first': 'первый', 'second': 'второй'}
+
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
+    inv_map = {v: k for k, v in dict.items()}
+    return inv_map
+
+print(reverse_dict(dict))
