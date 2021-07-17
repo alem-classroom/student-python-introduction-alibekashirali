eade1e87d974302142e1844540c8a7edd8339d28
Alibek Ashirali <ashiralialibek@gmail.com>
2021-07-17 15:15:22 +0600

    Update conditional.py

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -2,6 +2,7 @@ import random
 
 def is_positive(num):
     # return true if num is positive, otherwise return false
+    num = int(num)
     if num > 0:
         return True
     else:
@@ -9,6 +10,7 @@ def is_positive(num):
     
 def is_even(num):
     # return true if num is even, otherwise return false
+    num = int(num)
     if (num%2) == 0:
         return True
     else:
@@ -16,6 +18,7 @@ def is_even(num):
 
 def is_positive_and_even(num):
     # return true if num is positive and even, otherwise return false
+    num = int(num)
     if ((num%2) == 0) and num > 0:
         return True
     else:
@@ -23,6 +26,7 @@ def is_positive_and_even(num):
 
 def is_positive_or_even(num):
     # return true if num is positive or even, otherwise return false
+    num = int(num)
     if ((num%2) == 0) or num > 0:
         return True
     else:
